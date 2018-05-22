1508792d1223d321f6b44def5629bf02f734e5bc
Jonas Thuresson <thuresson.j@gmail.com>
2018-05-22 10:40:44 +0200

    dynamic pythonlib in setup

--- setup.py
+++ setup.py
@@ -150,6 +150,7 @@ inc_dirs = [s[2:] for s in opt.get('CHARM_CFLAGS').split() if s.startswith('-I')
 library_dirs = [s[2:] for s in opt.get('LDFLAGS').split() if s.startswith('-L')] + [s[2:] for s in opt.get('PY_LDFLAGS').split() if s.startswith('-L')]
 runtime_library_dirs = [s[11:] for s in opt.get('LDFLAGS').split()
                         if s.lower().startswith('-wl,-rpath,')]
+python_library = [s[2:] for s in opt.get('PY_LDFLAGS').split() if s.startswith('-lpython3')][:1]
 if opt.get('PAIR_MOD') == 'yes':
     if opt.get('USE_PBC') == 'yes':
         replaceString(lib_config_file, "pairing_lib=libs ", "pairing_lib=libs.pbc")
@@ -158,7 +159,7 @@ if opt.get('PAIR_MOD') == 'yes':
                                             benchmark_path] + inc_dirs,
                             sources = [math_path+'pairing/pairingmodule.c', 
                                         utils_path+'base64.c'],
-                            libraries=['pbc', 'gmp', 'crypto', 'python3.6m'], define_macros=_macros, undef_macros=_undef_macro,
+                            libraries=['pbc', 'gmp', 'crypto'] + python_library, define_macros=_macros, undef_macros=_undef_macro,
                             library_dirs=library_dirs, runtime_library_dirs=runtime_library_dirs)
 
     elif opt.get('USE_RELIC') == 'yes':
@@ -173,7 +174,7 @@ if opt.get('PAIR_MOD') == 'yes':
                             sources = [math_path + 'pairing/relic/pairingmodule3.c',
                                         math_path + 'pairing/relic/relic_interface.c',
                                         utils_path + 'base64.c'],
-                            libraries=['relic', 'gmp', 'crypto', 'python3.6m'], define_macros=_macros, undef_macros=_undef_macro,
+                            libraries=['relic', 'gmp', 'crypto'] + python_library, define_macros=_macros, undef_macros=_undef_macro,
                             library_dirs=library_dirs, runtime_library_dirs=runtime_library_dirs)
                             #extra_objects=[relic_lib], extra_compile_args=None)
 
@@ -188,7 +189,7 @@ if opt.get('PAIR_MOD') == 'yes':
                                             benchmark_path, miracl_inc],
                             sources = [math_path + 'pairing/miracl/pairingmodule2.c',
                                         math_path + 'pairing/miracl/miracl_interface2.cc'],
-                            libraries=['gmp', 'crypto', 'stdc++', 'python3.6m'], define_macros=_macros, undef_macros=_undef_macro,
+                            libraries=['gmp', 'crypto', 'stdc++'] + python_library, define_macros=_macros, undef_macros=_undef_macro,
                             extra_objects=[miracl_lib], extra_compile_args=None,
                             library_dirs=library_dirs, runtime_library_dirs=runtime_library_dirs)
 
@@ -201,7 +202,7 @@ if opt.get('INT_MOD') == 'yes':
                                             benchmark_path] + inc_dirs,
                             sources = [math_path + 'integer/integermodule.c', 
                                         utils_path + 'base64.c'], 
-                            libraries=['gmp', 'crypto', 'python3.6m'], define_macros=_macros, undef_macros=_undef_macro,
+                            libraries=['gmp', 'crypto'] + python_library, define_macros=_macros, undef_macros=_undef_macro,
                             library_dirs=library_dirs, runtime_library_dirs=runtime_library_dirs)
    _ext_modules.append(integer_module)
    
@@ -212,7 +213,7 @@ if opt.get('ECC_MOD') == 'yes':
                                 benchmark_path] + inc_dirs,
 				sources = [math_path + 'elliptic_curve/ecmodule.c',
                             utils_path + 'base64.c'], 
-				libraries=['gmp', 'crypto', 'python3.6m'], define_macros=_macros, undef_macros=_undef_macro,
+				libraries=['gmp', 'crypto'] + python_library, define_macros=_macros, undef_macros=_undef_macro,
                 library_dirs=library_dirs, runtime_library_dirs=runtime_library_dirs)
    _ext_modules.append(ecc_module)
 
@@ -220,20 +221,20 @@ benchmark_module = Extension(core_prefix + '.benchmark',
                 sources = [benchmark_path + 'benchmarkmodule.c'],
                 include_dirs=inc_dirs,
                 library_dirs=library_dirs,
-                libraries=['python3.6m'],
+                libraries=[] + python_library,
                 runtime_library_dirs=runtime_library_dirs)
 
 cryptobase = Extension(crypto_prefix+'.cryptobase', sources = [cryptobase_path + 'cryptobasemodule.c'],
                     include_dirs = [cryptobase_path]+inc_dirs,
                     library_dirs=library_dirs,
-                    libraries=['python3.6m'],
+                    libraries=[] + python_library,
                     runtime_library_dirs=runtime_library_dirs)
 
 aes = Extension(crypto_prefix + '.AES',
                     include_dirs = [cryptobase_path]+inc_dirs,
                     sources = [crypto_path + 'AES/AES.c'],
                     library_dirs=library_dirs,
-                    libraries=['python3.6m'],
+                    libraries=[] + python_library,
                     runtime_library_dirs=runtime_library_dirs)
 
 des  = Extension(crypto_prefix + '.DES',
@@ -241,7 +242,7 @@ des  = Extension(crypto_prefix + '.DES',
                                     cryptobase_path] + inc_dirs,
                     sources = [crypto_path + 'DES/DES.c'],
                     library_dirs=library_dirs,
-                    libraries=['python3.6m'],
+                    libraries=[] + python_library,
                     runtime_library_dirs=runtime_library_dirs)
 
 des3  = Extension(crypto_prefix + '.DES3',
@@ -250,7 +251,7 @@ des3  = Extension(crypto_prefix + '.DES3',
                                     crypto_path + 'DES/'] + inc_dirs, 
                     sources = [crypto_path + 'DES3/DES3.c'],
                     library_dirs = library_dirs,
-                    libraries=['python3.6m'],
+                    libraries=[] + python_library,
                     runtime_library_dirs=runtime_library_dirs)
 
 _ext_modules.extend([benchmark_module, cryptobase, aes, des, des3])
